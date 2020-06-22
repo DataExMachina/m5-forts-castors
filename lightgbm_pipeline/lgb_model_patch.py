089ed7f809d531892809e7d5337b57b99505ec6f
DataExMachina <binvignat.j@gmail.com>
2020-06-22 09:57:14 +0200

    feat: patch lgb .txt to .pickle

---
+++ lightgbm_pipeline/lgb_model_patch.py
@@ -0,0 +1,12 @@
+
+import lightgbm as lgb
+from glob import glob
+import pickle 
+
+for model_path in glob('./data/models/*_lgb.txt'):
+	model_new_path = model_path.replace('txt', 'pickle')
+	m_lgb = lgb.Booster(model_file=model_path)
+
+	pickle_out = open(model_new_path, "wb")
+	pickle.dump(m_lgb, pickle_out)	
+	
